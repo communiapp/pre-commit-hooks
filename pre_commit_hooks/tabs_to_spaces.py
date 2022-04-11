6ad6b4dc60ff1212a66f7b7d81d9ff453a65f3c6
Micha Preußer <micha@communiapp.de>
2022-04-11 18:09:33 +0200

    added encoding

--- pre_commit_hooks/tabs_to_spaces.py
+++ pre_commit_hooks/tabs_to_spaces.py
@@ -9,7 +9,7 @@ def check_file(filename: str) -> bool:
     for line in lines:
         if b"\t" in line:
             fixed_tabs = fixed_tabs + 1
-        newfile = newfile + line.replace(b"\t", bytes("    ")) + "\n"
+        newfile = newfile + line.replace(b"\t", bytes("    ", encoding='utf8')) + "\n"
     file.close()
     file_out = open(filename, mode='wb')
     file_out.write(newfile)
