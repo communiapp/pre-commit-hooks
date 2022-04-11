4ac9f93e10b9975ba9fce47097595111093ad1af
Micha Preußer <micha@communiapp.de>
2022-04-11 18:05:28 +0200

    cast replace string to byte

--- pre_commit_hooks/tabs_to_spaces.py
+++ pre_commit_hooks/tabs_to_spaces.py
@@ -9,7 +9,7 @@ def check_file(filename: str) -> bool:
     for line in lines:
         if "\t" in line:
             fixed_tabs = fixed_tabs + 1
-        newfile = newfile + line.replace("\t", "    ") + "\n"
+        newfile = newfile + line.replace(b"\t", bytes("    ")) + "\n"
     file.close()
     file_out = open(filename, mode='wb')
     file_out.write(newfile)
