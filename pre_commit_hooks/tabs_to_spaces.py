4fdc2f6aef38010a361a0a2d61855d880644f8db
Micha Preußer <micha@communiapp.de>
2022-04-12 08:40:09 +0200

    use replace in whole file

--- pre_commit_hooks/tabs_to_spaces.py
+++ pre_commit_hooks/tabs_to_spaces.py
@@ -2,19 +2,12 @@ import argparse
 from typing import Sequence
 
 def check_file(filename: str) -> bool:
-    with open(filename, mode='rb') as file:
-        lines = file.readlines()
-    newfile = ""
-    fixed_tabs = 0
-    for line in lines:
-        if b"\t" in line:
-            fixed_tabs = fixed_tabs + 1
-        newfile = newfile + line.replace(b"\t", bytes("    ", encoding='utf8')) + "\n"
-    file.close()
-    file_out = open(filename, mode='wb')
-    file_out.write(newfile)
-    file_out.close()
-    return fixed_tabs
+    with open(filename, mode='r') as file:
+        content = file.read()
+    newcontent = content.replace("\t", "    ")
+    with open(filename, mode='w') as file:
+        file.write(newcontent)
+    return newcontent != content
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
@@ -23,5 +16,5 @@ def main(argv: Sequence[str] | None = None) -> int:
     for filename in args.filenames:
         fixed_tabs = check_file(filename)
         if fixed_tabs:
-            print(f'{fixed_tabs} fixed tabs in {filename}')
+            print(f'Fixed tabs in {filename}')
     return 0
